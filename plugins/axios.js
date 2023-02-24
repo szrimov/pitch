a4516f388bd3315d83fb751d0f8d3a9cbdb7d311
yarncra <yarncra@gmail.com>
2023-02-25 01:09:47 +0300

    1000 test eslint

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,5 @@
-export default function ({
-  $axios
-}) {
-  $axios.interceptors.request.use((config) => {
+export default function ({$axios}) {
+  $axios.interceptors.request.use(config => {
     config.headers.Account = '8b59a775077536f3b73bb774655b94f6'
     return config
   })
