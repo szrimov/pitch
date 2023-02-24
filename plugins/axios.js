122b52d22116a9aab9e3d0b1c9f6e3ee9b635659
yarncra <yarncra@gmail.com>
2023-02-25 01:16:17 +0300

    1000 test

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,4 @@
-export default function ({$axios}) {
+export default function({$axios}) {
   $axios.interceptors.request.use(config => {
     config.headers.Account = '8b59a775077536f3b73bb774655b94f6'
     return config
