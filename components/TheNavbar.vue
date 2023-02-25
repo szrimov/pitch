acb8b157dc2543b8a00bd2da51c3fe48499659cf
yarncra <yarncra@gmail.com>
2023-02-25 16:26:18 +0300

    1000 test

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -3,15 +3,24 @@
     <div
       class="container d-flex justify-content-between align-items-center w-100"
     >
-      <nuxt-link class="navbar-brand" to="/"> Главная </nuxt-link>
-      <ul v-if="!$auth.loggedIn" class="navbar-nav">
+      <nuxt-link
+class="navbar-brand"
+to="/"> Главная </nuxt-link>
+      <ul
+v-if="!$auth.loggedIn"
+class="navbar-nav">
         <li class="nav-item">
-          <nuxt-link active-class="active" class="nav-link" to="/login">
+          <nuxt-link
+active-class="active"
+class="nav-link"
+to="/login">
             Авторизация
           </nuxt-link>
         </li>
       </ul>
-      <ul v-else class="navbar-nav">
+      <ul
+v-else
+class="navbar-nav">
         <li class="nav-item">
           <nuxt-link
             exact
@@ -24,7 +33,10 @@
           </nuxt-link>
         </li>
         <li class="nav-item">
-          <nuxt-link active-class="active" class="nav-link" to="/favourites">
+          <nuxt-link
+active-class="active"
+class="nav-link"
+to="/favourites">
             Избранное
           </nuxt-link>
         </li>
