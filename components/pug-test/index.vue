7a2f891ce22540bb9a646ed10531222ac2f068f7
yarncra <yarncra@gmail.com>
2023-02-25 19:41:35 +0300

    1000 test pug

--- components/pug-test/index.vue
+++ components/pug-test/index.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
-    h1 hello
+h1(value='2', data='2', data='2', @click='hello') hello
 </template>
 
 <script>
 export default {
-  name: 'pug-test-component'
+  name: 'pug-test-component',
+  methods: {
+    hello() {
+      return 'hello'
+    }
+  }
 }
 </script>
 
