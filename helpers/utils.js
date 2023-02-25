e73c4d61b2219935e620a2794108d92da1e1e071
yarncra <yarncra@gmail.com>
2023-02-25 16:42:04 +0300

    1000 test

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,2 +1,4 @@
-export const range = (start, end) =>
-  [...Array(end).keys()].map(el => el + start)
+export const range = (start, end) => {
+  console.log(1)
+  return [...Array(end).keys()].map(el => el + start)
+}
